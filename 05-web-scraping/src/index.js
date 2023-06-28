3c38890d1de679b12095c4abf798cf871e5911b7
Andrea Ferrer <andreaferrer361@gmail.com>
2023-06-28 13:10:46 +0200

    aaa

--- 05-web-scraping/src/index.js
+++ 05-web-scraping/src/index.js
@@ -2,3 +2,18 @@ import jsdom from "jsdom";
 import fetch from "isomorphic-fetch"
 import puppeteer from "puppeteer"
 
+(async () => {
+    // Lancement browser
+      const browser = await puppeteer.launch();
+    //Ouvrir une nouvelle page
+      const page = await browser.newPage();
+    //Aller au lien example.com
+    await page.goto('https://fr.wikipedia.org/wiki/Canton_(Suisse)#Donn%C3%A9es_cantonales');
+
+    //Récupérer le contenu de la page
+    const content = await page.content();
+
+    const screenshot = await page.screenshot();
+
+    await browser.close();
+});
